f3605026bd2c86e67200408f95e39b6bc5036819
mfteloglu <mfteloglu@gmail.com>
2023-11-20 16:20:36 +0100

    initial commit
    copy the module template via copier

---
+++ parma_mining/peopledatalabs/__init__.py
@@ -0,0 +1,8 @@
+"""peopledatalabs data sourcing package."""
+
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version(__name__)
+except Exception:
+    __version__ = "unknown"
