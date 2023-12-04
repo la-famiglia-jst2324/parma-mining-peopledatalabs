c68d8bb1d752d241656edc168ceb9ac8a1197172
mfteloglu <mfteloglu@gmail.com>
2023-12-04 14:35:30 +0100

    test: add unit test

---
+++ tests/test_get_organization_details.py
@@ -0,0 +1,150 @@
+import pytest
+from unittest.mock import MagicMock
+
+from fastapi.testclient import TestClient
+from parma_mining.peopledatalabs.api.main import app
+
+client = TestClient(app)
+
+
+@pytest.fixture
+def mock_pdl_client(mocker) -> MagicMock:
+    mock = mocker.patch(
+        "parma_mining.peopledatalabs.api.main.PdlClient.get_organization_details"
+    )
+    mock.return_value = {
+        "status": 200,
+        "name": "test",
+        "display_name": "test",
+        "size": "test",
+        "employee_count": 1,
+        "id": "test",
+        "founded": 1,
+        "industry": "test",
+        "naics": None,
+        "sic": None,
+        "location": {
+            "name": "test",
+            "locality": "test",
+            "region": "test",
+            "metro": "test",
+            "country": "test",
+            "continent": "test",
+            "street_address": "test",
+            "address_line_2": "test",
+            "postal_code": "test",
+            "geo": "test",
+        },
+        "linkedin_id": "test",
+        "linkedin_url": "test",
+        "facebook_url": "test",
+        "twitter_url": "test",
+        "profiles": [],
+        "website": "test",
+        "ticker": "test",
+        "gics_sector": "test",
+        "mic_exchange": "test",
+        "type": "test",
+        "summary": "test",
+        "tags": [],
+        "headline": "test",
+        "alternative_names": [],
+        "alternative_domains": [],
+        "affiliated_profiles": [],
+        "likelihood": 5,
+    }
+    return mock
+
+
+def test_get_organization_details(mock_pdl_client: MagicMock):
+    payload = {
+        "companies": {"google": ["google"], "facebook": ["facebook"]},
+        "type": "name",
+    }
+    response = client.post("/organizations", json=payload)
+    print(response.json())
+    assert response.status_code == 200
+    assert response.json() == [
+        {
+            "status": 200,
+            "name": "test",
+            "display_name": "test",
+            "size": "test",
+            "employee_count": 1,
+            "id": "test",
+            "founded": 1,
+            "industry": "test",
+            "naics": None,
+            "sic": None,
+            "location": {
+                "name": "test",
+                "locality": "test",
+                "region": "test",
+                "metro": "test",
+                "country": "test",
+                "continent": "test",
+                "street_address": "test",
+                "address_line_2": "test",
+                "postal_code": "test",
+                "geo": "test",
+            },
+            "linkedin_id": "test",
+            "linkedin_url": "test",
+            "facebook_url": "test",
+            "twitter_url": "test",
+            "profiles": [],
+            "website": "test",
+            "ticker": "test",
+            "gics_sector": "test",
+            "mic_exchange": "test",
+            "type": "test",
+            "summary": "test",
+            "tags": [],
+            "headline": "test",
+            "alternative_names": [],
+            "alternative_domains": [],
+            "affiliated_profiles": [],
+            "likelihood": 5,
+        },
+        {
+            "status": 200,
+            "name": "test",
+            "display_name": "test",
+            "size": "test",
+            "employee_count": 1,
+            "id": "test",
+            "founded": 1,
+            "industry": "test",
+            "naics": None,
+            "sic": None,
+            "location": {
+                "name": "test",
+                "locality": "test",
+                "region": "test",
+                "metro": "test",
+                "country": "test",
+                "continent": "test",
+                "street_address": "test",
+                "address_line_2": "test",
+                "postal_code": "test",
+                "geo": "test",
+            },
+            "linkedin_id": "test",
+            "linkedin_url": "test",
+            "facebook_url": "test",
+            "twitter_url": "test",
+            "profiles": [],
+            "website": "test",
+            "ticker": "test",
+            "gics_sector": "test",
+            "mic_exchange": "test",
+            "type": "test",
+            "summary": "test",
+            "tags": [],
+            "headline": "test",
+            "alternative_names": [],
+            "alternative_domains": [],
+            "affiliated_profiles": [],
+            "likelihood": 5,
+        },
+    ]
