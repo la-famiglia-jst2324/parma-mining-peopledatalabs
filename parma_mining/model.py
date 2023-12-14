3830ff643f20a5ed532da3fcc1e40611f7b69c1b
Ege Kocabas <kocabas.ege@tum.de>
2023-12-14 05:58:38 +0100

    chore: Fix ruff and add stricter linting

--- parma_mining/model.py
+++ parma_mining/model.py
@@ -1,35 +1,35 @@
-from typing import Any, List, Optional, Dict
+from typing import Any
 
 from pydantic import BaseModel
 
 
 class Naic(BaseModel):
-    naics_code: Optional[str]
-    sector: Optional[str]
-    sub_sector: Optional[str]
-    industry_group: Optional[str]
-    naics_industry: Optional[str]
-    national_industry: Optional[str]
+    naics_code: str | None
+    sector: str | None
+    sub_sector: str | None
+    industry_group: str | None
+    naics_industry: str | None
+    national_industry: str | None
 
 
 class SicItem(BaseModel):
-    sic_code: Optional[str]
-    major_group: Optional[str]
-    industry_group: Optional[str]
-    industry_sector: Optional[str]
+    sic_code: str | None
+    major_group: str | None
+    industry_group: str | None
+    industry_sector: str | None
 
 
 class Location(BaseModel):
-    name: Optional[str]
-    locality: Optional[str]
-    region: Optional[str]
-    metro: Optional[str]
-    country: Optional[str]
-    continent: Optional[str]
-    street_address: Optional[str]
-    address_line_2: Optional[str]
-    postal_code: Optional[str]
-    geo: Optional[str]
+    name: str | None
+    locality: str | None
+    region: str | None
+    metro: str | None
+    country: str | None
+    continent: str | None
+    street_address: str | None
+    address_line_2: str | None
+    postal_code: str | None
+    geo: str | None
 
 
 class OrganizationModel(BaseModel):
@@ -40,34 +40,34 @@ class OrganizationModel(BaseModel):
 
     status: int
     name: str
-    display_name: Optional[str]
-    size: Optional[str]
-    employee_count: Optional[int]
+    display_name: str | None
+    size: str | None
+    employee_count: int | None
     id: str
-    founded: Optional[int]
-    industry: Optional[str]
-    naics: Optional[List[Naic]]
-    sic: Optional[List[SicItem]]
-    location: Optional[Location]
-    linkedin_id: Optional[str]
-    linkedin_url: Optional[str]
-    facebook_url: Optional[str]
-    twitter_url: Optional[str]
-    profiles: Optional[List[str]]
-    website: Optional[str]
-    ticker: Optional[str]
-    gics_sector: Optional[Any]
-    mic_exchange: Optional[Any]
-    type: Optional[str]
-    summary: Optional[str]
-    tags: Optional[List[str]]
-    headline: Optional[str]
-    alternative_names: Optional[List[str]]
-    alternative_domains: Optional[List[str]]
-    affiliated_profiles: Optional[List[str]]
-    likelihood: Optional[int]
+    founded: int | None
+    industry: str | None
+    naics: list[Naic] | None
+    sic: list[SicItem] | None
+    location: Location | None
+    linkedin_id: str | None
+    linkedin_url: str | None
+    facebook_url: str | None
+    twitter_url: str | None
+    profiles: list[str] | None
+    website: str | None
+    ticker: str | None
+    gics_sector: Any | None
+    mic_exchange: Any | None
+    type: str | None
+    summary: str | None
+    tags: list[str] | None
+    headline: str | None
+    alternative_names: list[str] | None
+    alternative_domains: list[str] | None
+    affiliated_profiles: list[str] | None
+    likelihood: int | None
 
 
 class CompaniesRequest(BaseModel):
-    companies: Dict[str, List[str]]
+    companies: dict[str, list[str]]
     type: str
--- parma_mining/peopledatalabs/api/main.py
+++ parma_mining/peopledatalabs/api/main.py
@@ -1,12 +1,12 @@
 """Main entrypoint for the API routes in of parma-analytics."""
 
-from fastapi import FastAPI, status
-from dotenv import load_dotenv
 import os
-from typing import List
-from parma_mining.client import PdlClient
-from parma_mining.model import OrganizationModel, CompaniesRequest
 
+from dotenv import load_dotenv
+from fastapi import FastAPI, status
+
+from parma_mining.client import PdlClient
+from parma_mining.model import CompaniesRequest, OrganizationModel
 
 load_dotenv()
 
@@ -27,18 +27,18 @@ def root():
 @app.post(
     "/organizations",
     status_code=status.HTTP_200_OK,
-    response_model=List[OrganizationModel],
+    response_model=list[OrganizationModel],
 )
-def get_organization_details(companies: CompaniesRequest) -> List[OrganizationModel]:
+def get_organization_details(companies: CompaniesRequest) -> list[OrganizationModel]:
     """API Endpoint for the organization details according to the company domains.
 
     Possible types : "name" and "website"
     """
-    _pdlClient = PdlClient(api_key, api_version, base_url)
+    _pdl_client = PdlClient(api_key, api_version, base_url)
     all_org_details = []
     for company_name, company_identifiers in companies.companies.items():
         for company_identifier in company_identifiers:
-            org_details = _pdlClient.get_organization_details(
+            org_details = _pdl_client.get_organization_details(
                 company_identifier, companies.type
             )
             all_org_details.append(org_details)
