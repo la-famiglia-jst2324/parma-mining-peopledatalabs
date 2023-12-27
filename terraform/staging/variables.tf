f71447afb435912406dea3dbdab5373a0ae36fa8
Ege Kocabas <48245934+egekocabas@users.noreply.github.com>
2023-12-27 22:40:55 +0100

    refactor: Remove Firebase Adminsdk (#16)

--- terraform/staging/variables.tf
+++ terraform/staging/variables.tf
@@ -1,9 +1,3 @@
-variable "FIREBASE_ADMINSDK_CERTIFICATE" {
-  description = "value"
-  type        = string
-  sensitive   = true
-}
-
 variable "PDL_API_KEY" {
   description = "value"
   type        = string
