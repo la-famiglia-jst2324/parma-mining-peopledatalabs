33daf275530a9f0e0288c610386310fe9d4b544e
Ege Kocabas <48245934+egekocabas@users.noreply.github.com>
2024-01-10 01:36:43 +0100

    refactor: Support different log levels for different environments (#23)

--- parma_mining/peopledatalabs/api/main.py
+++ parma_mining/peopledatalabs/api/main.py
@@ -8,7 +8,15 @@ from fastapi import FastAPI, status
 from parma_mining.client import PdlClient
 from parma_mining.model import CompaniesRequest, OrganizationModel
 
-logging.basicConfig(level=logging.INFO)
+env = os.getenv("env", "local")
+
+if env == "prod":
+    logging.basicConfig(level=logging.INFO)
+elif env in ["staging", "local"]:
+    logging.basicConfig(level=logging.DEBUG)
+else:
+    logging.warning(f"Unknown environment '{env}'. Defaulting to INFO level.")
+    logging.basicConfig(level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
